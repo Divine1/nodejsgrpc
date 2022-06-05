fcd978ee50efe0ba6046f869ab374d197961b134
Divine <dchelladurai@vmware.com>
2022-06-05 13:44:00 +0530

    updated comment for unary feature in grpc

--- greet/server/service_impl.js
+++ greet/server/service_impl.js
@@ -1,6 +1,6 @@
 const pb = require("./../proto/greet_pb");
 
-
+// this method name should be same as the package name present in greet.proto file
 exports.greet = (call,callback)=>{
     console.log("greet was invoked")
     const res = new pb.GreetResponse()
@@ -8,3 +8,4 @@ exports.greet = (call,callback)=>{
 
     callback(null,res);
 }
+
