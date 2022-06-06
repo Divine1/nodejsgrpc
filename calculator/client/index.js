4655f7ff2dd977121baa04f9c93e84c0a8773fbd
Divine <dchelladurai@vmware.com>
2022-06-06 23:15:40 +0530

    example - server streaming

--- calculator/client/index.js
+++ calculator/client/index.js
@@ -2,6 +2,7 @@ const grpc = require("@grpc/grpc-js")
 
 const {CalculatorServiceClient} = require("../proto/calculator_grpc_pb");
 const {SumRequest} = require("./../proto/sum_pb")
+const {PrimeRequest} = require("./../proto/primes_pb");
 function doSum(client){
     console.log("doCalculator was invoked")
     const req = new SumRequest().setFirstNumber(1)
@@ -17,11 +18,21 @@ function doSum(client){
     })
 }
 
+function doPrimes(client){
+    const req = new PrimeRequest().setNumber(12390392840);
+    const call = client.primes(req);
+
+    call.on("data",(res)=>{
+        console.log("primtes ",res.getResult())
+    })
+}
+
 function main(){
     const creds = grpc.ChannelCredentials.createInsecure();
     const client = new CalculatorServiceClient("localhost:50051",creds);
 
-    doSum(client);
+    //doSum(client);
+    doPrimes(client);
     client.close();
 }
 main();
--- calculator/server/service_impl.js
+++ calculator/server/service_impl.js
@@ -1,5 +1,5 @@
 const {SumResponse} = require("./../proto/sum_pb.js");
-
+const {PrimeResponse} = require("./../proto/primes_pb");
 // this method name should be same as the package name present in greet.proto file
 exports.sum = (call,callback)=>{
     console.log("calculator was invoked")
@@ -11,3 +11,24 @@ exports.sum = (call,callback)=>{
     callback(null,res);
 }
 
+
+
+
+exports.primes = (call,_)=>{
+    console.log("primes was invoked");
+    let number = call.request.getNumber();
+    let divisor =2;
+    const res = new PrimeResponse();
+
+    while(number >1){
+        if(number % divisor ==0){
+            res.setResult(divisor);
+            call.write(res);
+            number /=divisor;
+        }
+        else{
+            ++divisor;
+        }
+    }
+    call.end();
+}
